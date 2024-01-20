7a2536d0afe689be82a48738f11b8d7affdbccba
KUSATAKE Daisuke <me@kusatake.dev>
2024-01-20 12:23:58 +0900

    Layout   - TheContainer     - ThePanel     - TheScore

---
+++ src/components/TheContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <!-- Content goes here -->
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.container {
+  @apply mx-auto max-w-7xl sm:px-6 lg:px-8;
+}
+</style>
---
+++ src/components/ThePanel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="panel">
+    <div class="panel-header">
+      <!-- Content goes here -->
+      <!-- We use less vertical padding on card headers on desktop than on body sections -->
+      <slot name="header"></slot>
+    </div>
+    <div class="panel-body">
+      <!-- Content goes here -->
+      <slot name="default"></slot>
+    </div>
+    <div class="panel-footer">
+      <!-- Content goes here -->
+      <!-- We use less vertical padding on card footers at all sizes than on headers or body sections -->
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.panel {
+  @apply divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow;
+}
+.panel-header {
+  @apply px-4 py-5 sm:px-6
+}
+.panel-body {
+  @apply px-4 py-5 sm:p-6
+}
+.panel-footer {
+  @apply px-4 py-4 sm:px-6
+}
+</style>
